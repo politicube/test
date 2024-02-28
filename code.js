d4a39a55025b72fecd8f9d0ecc8bbabc9d800a3e
politigram-compass <politigram-compass@users.noreply.github.com>
2024-02-27 22:39:14 -0500

    "more" labels

--- code.js
+++ code.js
@@ -351,6 +351,16 @@ mode: 'markers',
 	type: 'scatter3d',
 	name: "Anarcho-Syndicalism"
 };
+var trace14 = {
+  x: ["10","0", "0","-10","0","0"],
+  y: ["0", "10", "0","0","-10","0"],
+    z:["0","0","10","0","0","-10"],
+  mode: 'text',
+  name: 'Labels',
+  text: ['More Right', 'More Authoritarian', 'More Conservative','More Left','More Libertarian','More Progressive'],
+  textposition: 'bottom',
+  type: 'scatter3d'
+};
 
 
 var data = [trace1,trace2,trace3,trace4,trace5,trace6,trace7,trace8,trace9,trace10,trace11,trace12,trace13,];
